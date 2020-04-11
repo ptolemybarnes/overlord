df75e4191310539c87720f58f51efbbbea1f3127
lucanioi <luca.nioi@badiapp.com>
2020-04-11 19:21:40 +0200

    use extend

--- overlord.rb
+++ overlord.rb
@@ -1,23 +1,26 @@
 require 'pry'
 
 module Overlord
-  INCLUDED = Proc.new do |method_name|
-    @overloaded_methods ||= Hash.new
-    method = instance_method(method_name)
-    return if method.arity < 0
-    undef_method method_name
-    @overloaded_methods[[method_name, method.arity].hash] = method
+  module ClassMethods
+    def method_added(method_name)
+      method = instance_method(method_name)
+      return if method.arity < 0
+      undef_method method_name
+      overloaded_methods[[method_name, method.arity].hash] = method
 
-    overloaded_methods = @overloaded_methods
+      define_method(method_name) do |*args|
+        method = self.class.overloaded_methods[[method_name, args.size].hash]
+        raise NoMethodError.new("undefined method `#{method_name}` with arity #{args.size}") unless method
+        method.bind(self).call(*args)
+      end
+    end
 
-    define_method(method_name) do |*args|
-      method = overloaded_methods[[method_name, args.size].hash]
-      raise NoMethodError.new("undefined method `#{method_name}` with arity #{args.size}") unless method
-      method.bind(self).call(*args)
+    def overloaded_methods
+      @overloaded_methods ||= {}
     end
   end
 
   def self.included(klass)
-    klass.define_singleton_method(:method_added, &INCLUDED)
+    klass.extend(ClassMethods)
   end
 end
